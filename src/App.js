c2698b61a31a4f4d731fedf58b0dffa12b0c3f13
seoseongmin96 <justin090452@gmail.com>
2022-07-13 18:00:44 +0900

    모든 계이름 console 찍힘

--- src/App.js
+++ src/App.js
@@ -55,34 +55,196 @@ function App(){
     const Db4 = playDb4()
     alert(`클릭되는 음 : ${Db4}`)
 }
+
+    const clickD4 =(e)=>{
+    e.preventDefault()
+    
+    const D4 = playD4()
+    alert(`클릭되는 음 : ${D4}`)
+  }
+
+  const clickEb4 =(e)=>{
+    e.preventDefault()
+    
+    const Eb4 = playEb4()
+    alert(`클릭되는 음 : ${Eb4}`)
+  }
+
+  const clickE4 =(e)=>{
+    e.preventDefault()
+    
+    const E4 = playE4()
+    alert(`클릭되는 음 : ${E4}`)
+  }
+
+  const clickF4 =(e)=>{
+    e.preventDefault()
+    
+    const F4 = playF4()
+    alert(`클릭되는 음 : ${F4}`)
+  }
+
+  const clickGb4 =(e)=>{
+    e.preventDefault()
+    
+    const Gb4 = playGb4()
+    alert(`클릭되는 음 : ${Gb4}`)
+  }
+
+  const clickG4 =(e)=>{
+    e.preventDefault()
+    
+    const G4 = playG4()
+    alert(`클릭되는 음 : ${G4}`)
+  }
+
+  const clickAb4 =(e)=>{
+    e.preventDefault()
+    
+    const Ab4 = playAb4()
+    alert(`클릭되는 음 : ${Ab4}`)
+  }
+
+  const clickA4 =(e)=>{
+    e.preventDefault()
+    
+    const A4 = playA4()
+    alert(`클릭되는 음 : ${A4}`)
+  }
+
+  const clickBb4 =(e)=>{
+    e.preventDefault()
+    
+    const Bb4 = playBb4()
+    alert(`클릭되는 음 : ${Bb4}`)
+  }
+
+  const clickB4 =(e)=>{
+    e.preventDefault()
+    
+    const B4 = playB4()
+    alert(`클릭되는 음 : ${B4}`)
+  }
+
+  const clickC5 =(e)=>{
+    e.preventDefault()
+    
+    const C5 = playC5()
+    alert(`클릭되는 음 : ${C5}`)
+  }
+
+  const clickDb5 =(e)=>{
+    e.preventDefault()
+    
+    const Db5 = playDb5()
+    alert(`클릭되는 음 : ${Db5}`)
+  }
+
+  const clickD5 =(e)=>{
+    e.preventDefault()
+    
+    const D5 = playD5()
+    alert(`클릭되는 음 : ${D5}`)
+  }
+
+  const clickEb5 =(e)=>{
+    e.preventDefault()
+    
+    const Eb5 = playEb5()
+    alert(`클릭되는 음 : ${Eb5}`)
+  }
+
+  const clickE5 =(e)=>{
+    e.preventDefault()
+    
+    const E5 = playE5()
+    alert(`클릭되는 음 : ${E5}`)
+  }
+
+  const clickF5 =(e)=>{
+    e.preventDefault()
+    
+    const F5 = playF5()
+    alert(`클릭되는 음 : ${F5}`)
+  }
+
+  const clickGb5 =(e)=>{
+    e.preventDefault()
+    
+    const Gb5 = playB4()
+    alert(`클릭되는 음 : ${Gb5}`)
+  }
+
+  const clickG5 =(e)=>{
+    e.preventDefault()
+    
+    const G5= playG5()
+    alert(`클릭되는 음 : ${G5}`)
+  }
+
+  const clickAb5 =(e)=>{
+    e.preventDefault()
+    
+    const Ab5 = playAb5()
+    alert(`클릭되는 음 : ${Ab5}`)
+  }
+
+  const clickA5 =(e)=>{
+    e.preventDefault()
+    
+    const A5 = playA5()
+    alert(`클릭되는 음 : ${A5}`)
+  }
+
+  const clickBb5 =(e)=>{
+    e.preventDefault()
+    
+    const Bb5 = playA5()
+    alert(`클릭되는 음 : ${Bb5}`)
+  }
+
+  const clickB5 =(e)=>{
+    e.preventDefault()
+    
+    const B5 = playA5()
+    alert(`클릭되는 음 : ${B5}`)
+  }
+
+  const clickC6 =(e)=>{
+    e.preventDefault()
+    
+    const C6 = playA5()
+    alert(`클릭되는 음 : ${C6}`)
+  }
     return(<div className='pianoPage'>
     <h1>Enjoy playing on the Tone Piano!</h1> <br/> <br/> <br/> <br/>
     <div className='piano'>
     <div className='white-key' onClick={clickC4}>A</div>
     <div className='black-key' onClick={clickDb4}>W</div>
-    <div className='white-key' onClick={playD4}>S</div>
-    <div className='black-key' onClick={playEb4}>E</div>
-    <div className='white-key' onClick={playE4}>D</div>
-    <div className='white-key' onClick={playF4}>F</div>
-    <div className='black-key' onClick={playGb4}>T</div>
-    <div className='white-key' onClick={playG4}>G</div>
-    <div className='black-key' onClick={playAb4}>Y</div>
-    <div className='white-key' onClick={playA4}>H</div>
-    <div className='black-key' onClick={playBb4}>U</div>
-    <div className='white-key' onClick={playB4}>J</div>
-    <div className='white-key' onClick={playC5}>K</div>
-    <div className='black-key' onClick={playDb5}>O</div>
-    <div className='white-key' onClick={playD5}>L</div>
-    <div className='black-key' onClick={playEb5}>P</div>
-    <div className='white-key' onClick={playE5}>Z</div>
-    <div className='white-key' onClick={playF5}>X</div>
-    <div className='black-key' onClick={playGb5}>C</div>
-    <div className='white-key' onClick={playG5}>V</div>
-    <div className='black-key' onClick={playAb5}>B</div>
-    <div className='white-key' onClick={playA5}>N</div>
-    <div className='black-key' onClick={playBb5}>M</div>
-    <div className='white-key' onClick={playB5}>R</div>
-    <div className='white-key' onClick={playC6}>I</div>
+    <div className='white-key' onClick={clickD4}>S</div>
+    <div className='black-key' onClick={clickEb4}>E</div>
+    <div className='white-key' onClick={clickE4}>D</div>
+    <div className='white-key' onClick={clickF4}>F</div>
+    <div className='black-key' onClick={clickGb4}>T</div>
+    <div className='white-key' onClick={clickG4}>G</div>
+    <div className='black-key' onClick={clickAb4}>Y</div>
+    <div className='white-key' onClick={clickA4}>H</div>
+    <div className='black-key' onClick={clickBb4}>U</div>
+    <div className='white-key' onClick={clickB4}>J</div>
+
+    <div className='white-key' onClick={clickC5}>K</div>
+    <div className='black-key' onClick={clickDb5}>O</div>
+    <div className='white-key' onClick={clickD5}>L</div>
+    <div className='black-key' onClick={clickEb5}>P</div>
+    <div className='white-key' onClick={clickE5}>Z</div>
+    <div className='white-key' onClick={clickF5}>X</div>
+    <div className='black-key' onClick={clickGb5}>C</div>
+    <div className='white-key' onClick={clickG5}>V</div>
+    <div className='black-key' onClick={clickAb5}>B</div>
+    <div className='white-key' onClick={clickA5}>N</div>
+    <div className='black-key' onClick={clickBb5}>M</div>
+    <div className='white-key' onClick={clickB5}>R</div>
+    <div className='white-key' onClick={clickC6}>I</div>
   
     </div>
     </div>)
--- src/tone.fn.js
+++ src/tone.fn.js
@@ -12,203 +12,247 @@ export function playDb4(keycode){
     return keycode
     }
     
-export function playD4(){
+export function playD4(keycode){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("D4","8n");
+    return keycode
     }   
     
-export function playEb4(){
+export function playEb4(keycode){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("Eb4","8n");
+    return keycode
     }   
 
-export function playE4(){
+export function playE4(keycode){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("E4","8n");
+    return keycode
     }
     
-export function playF4(){
+export function playF4(keycode){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("F4","8n");
+    return keycode
     }
         
-export function playGb4(){
+export function playGb4(keycode){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("Gb4","8n");
+    return keycode
     }
     
-export function playG4(){
+export function playG4(keycode){
     const synth = new Tone.Synth().toDestination();
-    
     synth.triggerAttackRelease("G4","8n");
+    return keycode
+   } 
     
-    } 
-    
-export function playAb4(){
+export function playAb4(keycode){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("Ab4","8n");
+    return keycode
     }
     
-export function playA4(){
+export function playA4(keycode){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("A4","8n");
+    return keycode
     }
 
-export function playBb4(){
+export function playBb4(keycode){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("Bb4","8n");
+    return keycode
     }
         
-export function playB4(){
+export function playB4(keycode){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("B4","8n");
+    return keycode
     }
 
-export function playC5(){
+export function playC5(keycode){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("C5","8n");
+    return keycode
     }
     
-export function playDb5(){
+export function playDb5(keycode){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("Db5","8n");
+    return keycode
     }
     
-export function playD5(){
+export function playD5(keycode){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("D5","8n");
+    return keycode
     }
         
-export function playEb5(){
+export function playEb5(keycode){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("Eb5","8n");
+    return keycode
     }
 
-export function playE5(){
+export function playE5(keycode){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("E5","8n");
+    return keycode
     }
 
-export function playF5(){
+export function playF5(keycode){
      const synth = new Tone.Synth().toDestination();
      synth.triggerAttackRelease("F5","8n");
+     return keycode
     }
 
-export function playGb5(){
+export function playGb5(keycode){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("Gb5","8n");
+    return keycode
     }
 
-export function playG5(){
+export function playG5(keycode){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("G5","8n");
+    return keycode
     }
 
-export function playAb5(){
+export function playAb5(keycode){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("Ab5","8n");
+    return keycode
     }
 
-export function playA5(){
+export function playA5(keycode){
     const synth = new Tone.Synth().toDestination();
      synth.triggerAttackRelease("A5","8n");
+     return keycode
     }
 
-export function playBb5(){
+export function playBb5(keycode){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("Bb5","8n");
+    return keycode
     }
 
-export function playB5(){
+export function playB5(keycode){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("B5","8n");
+    return keycode
     }
 
-export function playC6(){
+export function playC6(keycode){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("C6","8n");
+    return keycode
     }
 
 export function PlayNote(event){
     if(event.keyCode ===65){
-        alert('도')
+        alert('C4')
         playC4("A");
 
     }
     if(event.keyCode ===87){
-        alert('레b')
+        alert('Db4')
         playDb4("W");
     }
     if(event.keyCode ===83){
-        playD4();
+        alert('D4')
+        playD4("S");
     }
     if(event.keyCode ===69){
-        playEb4();
+        alert('Eb4')
+        playEb4("E");
     }
     if(event.keyCode ===68){
-        playE4();
+        alert('E4')
+        playE4("D");
     }
     if(event.keyCode ===70){
-        playF4();
+        alert('F4')
+        playF4("F");
     }
     if(event.keyCode ===84){
-        playGb4();
+        alert('Gb4')
+        playGb4("T");
     }
     if(event.keyCode ===71){
-        playG4();
+        alert('G4')
+        playG4("G");
     }
     if(event.keyCode ===89){
-        playAb4();
+        alert('Ab4')
+        playAb4("Y");
     }
     if(event.keyCode ===72){
-        playA4();
+        alert('A4')
+        playA4("H");
     }
     if(event.keyCode ===85){
-        playBb4();
+        alert('Bb4')
+        playBb4("U");
     }
     if(event.keyCode ===74){
-        playB4();
+        alert('B4')
+        playB4("J");
     }
 
 
     if(event.keyCode ===75){
-        playC5();
+        alert('C5')
+        playC5("K");
     }
     if(event.keyCode ===79){
-        playDb5();
+        alert('Db5')
+        playDb5("O");
     }
     if(event.keyCode ===76){
-        playD5();
+        alert('D5')
+        playD5("L");
     }
     if(event.keyCode ===80){
-        playEb5();
+        alert('Eb5')
+        playEb5("P");
     }
     if(event.keyCode ===90){
-        playE5();
+        alert('E5')
+        playE5("Z");
     }
     if(event.keyCode ===88){
-        playF5();
+        alert('F5')
+        playF5("X");
     }
     if(event.keyCode ===67){
-        playGb5();
+        alert('Gb5')
+        playGb5("C");
     }
     if(event.keyCode ===86){
-        playG5();
+        alert('G5')
+        playG5("V");
     }
     if(event.keyCode ===66){
-        playAb5();
+        alert('Ab5')
+        playAb5("B");
     }
     if(event.keyCode ===78){
-        playA5();
+        alert('A5')
+        playA5("N");
     }
     if(event.keyCode ===77){
-        playBb5();
+        alert('Bb5')
+        playBb5("M");
     }
     if(event.keyCode ===82){
-        playB5();
+        alert('B5')
+        playB5("R");
     }
     if(event.keyCode ===73){
-        playC6();
+        alert('C6')
+        playC6("I");
     }
 
     
